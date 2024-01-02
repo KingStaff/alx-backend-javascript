02d0678abd427f207e8ef91653367e9b643c0598
KarimStaff <karimawudulai@gmail.com>
2024-01-02 22:41:45 +0000

    Task10 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newarray = [];
-  for (let value of array) {
-    newarray.push(appendString + value);
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
-  return newarray;
+  return newArray;
 }
