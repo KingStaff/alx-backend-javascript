0c8f581e75e78cbe8b4e774a2eb448f85ff31f79
KarimStaff <karimawudulai@gmail.com>
2024-01-09 12:52:39 +0000

    Task 10-update_uniq_items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,6 @@
-/**
- * Changes the quantity of unique grocery items to 100.
- * @param {Map<String, number>} map - A map of the name of a
- * grocery and its quantity.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
+    throw new Error("Cannot process");
   }
   map.forEach((value, key) => {
     if (value === 1) {
